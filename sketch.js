f15e70cbfd818b42b92ee2cf0dd701c4ad384adc
test_3 <810828@seq.org>
2022-01-05 14:03:16 -0800

    adding more stuff

--- sketch.js
+++ sketch.js
@@ -11,9 +11,9 @@ function setup() {
   var cnv = createCanvas(600, 600);
   cnv.position((windowWidth-width)/2, 30);
   loadCell();
+  // runCell();
   // console.log(arr);
   console.log(arr[2][5]);
-  runCell()
 }
 
 function draw() {
@@ -28,7 +28,8 @@ function loadCell() {
   
   for(let i = 0; i < arr.length; i++) {
     for(let j = 0; j < arr.length; j++) {
-        arr[i][j] = new Cell( 10, 10);
+        arr[i][j] = new Cell( i, j);
+        arr[i][j].run()
         // cell[i][j].run()
     }
   }
@@ -36,7 +37,7 @@ function loadCell() {
 }
 
 function runCell(){
-  for(let h = 0; h < arr.length; h++) {
-    cell[h].run()
+  for(let h = 0; h < 20; h++) {
+    // cell[h].run()
   }
 }
